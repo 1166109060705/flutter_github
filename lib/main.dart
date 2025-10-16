06b227ca83bea014d2170d6acd2a27730afde2a4
Teeradech1166109060705 <156157381+1166109060705@users.noreply.github.com>
2025-10-16 11:41:08 +0700

    Update: แก้ไขข้อความเป็นชื่อตัวเอง

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('สวัสดีทุกคน'),
+          child: Text('โค้ดของธีรเดช'),
         ),
       ),
     );
